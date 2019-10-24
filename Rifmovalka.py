6a914f642efbf3d2b5fde00f873b13a7a7accb94
Maxim Maltsev <55740776+Maxim963@users.noreply.github.com>
2019-10-24 17:40:56 +0300

    Update Rifmovalka.py

--- Rifmovalka.py
+++ Rifmovalka.py
@@ -11,4 +11,4 @@ while True:
             if g in i[len(g)*(-1)-1:] and str(g) != str(i):
                 Thread(cou.append(g))
                 Thread(print(i.title(), end= '- '))
-    print('Найдено: %i рифм %s'%(len(cou), '\n'))
+    pprint('Найдено: %i рифм %s'%(len(cou), '\n'))
